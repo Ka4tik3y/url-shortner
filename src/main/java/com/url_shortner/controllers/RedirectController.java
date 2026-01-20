702dd58af2f684dcd0c0d3c0dd3613a8042799ae
Kartikey <kartikey442004@gmail.com>
2026-01-20 14:45:45 +0530

    added the link redirect functionality and fechted the click event analytics most the shortning and analytics parts are done

---
+++ src/main/java/com/url_shortner/controllers/RedirectController.java
@@ -0,0 +1,29 @@
+package com.url_shortner.controllers;
+
+import com.url_shortner.models.UrlMapping;
+import com.url_shortner.services.UrlMappingService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class RedirectController {
+
+    private UrlMappingService urlMappingService;
+@GetMapping("/{shortUrl}")
+    public ResponseEntity<Void> redirect(@PathVariable String shortUrl){
+        UrlMapping urlMapping = urlMappingService.getOriginalUrl(shortUrl);
+        if(urlMapping != null){
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Location", urlMapping.getOriginalUrl());
+            return ResponseEntity.status(302).headers(headers).build();
+        }else{
+        return ResponseEntity.notFound().build();
+        }
+    }
+}
--- src/main/java/com/url_shortner/services/UrlMappingService.java
+++ src/main/java/com/url_shortner/services/UrlMappingService.java
@@ -89,4 +89,18 @@ public class UrlMappingService {
        return clickEvents.stream()
                .collect(Collectors.groupingBy(click -> click.getClickDate().atStartOfDay().toLocalDate(), Collectors.counting()));
     }
+
+    public UrlMapping getOriginalUrl(String shortUrl) {
+          UrlMapping urlMapping = urlMappingRepository.findByShortUrl(shortUrl);
+          if(urlMapping != null) {
+              urlMapping.setClickCount(urlMapping.getClickCount() + 1);
+              urlMappingRepository.save(urlMapping);
+          ClickEvent clickEvent = new ClickEvent();
+          clickEvent.setClickDate(LocalDate.now());
+          clickEvent.setUrlMapping(urlMapping);
+          clickEventRepository.save(clickEvent);
+          }
+          return urlMapping;
+
+    }
 }
